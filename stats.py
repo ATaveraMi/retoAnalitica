a344087e4149523c1686d057e00abf7aca68db84
Tavernauta <andrestavera03@gmail.com>
2023-03-21 15:25:48 -0600

    s

--- stats.py
+++ stats.py
@@ -1,4 +1,17 @@
 import pandas as pd
 data = pd.read_csv("/Users/andrestavera/Documents/GitHub/retoAnalitica/avocado.csv")
-print(data)
-print(data.rows)
+
+print("data", data.head)
+print(data.iloc[15,2])
+
+
+print(data.sort_values(by="AveragePrice"))
+
+rows= len(data.axes[0])
+cols= len(data.axes[1])
+print(f"rows: {rows}\ncols {cols}")
+
+
+print(data["AveragePrice"].max())
+
+print(data["AveragePrice"].min())
